ecd9807d91e3bc77431f30af72e888fc6236a9ed
Fabio Rehm <fgrehm@gmail.com>
2012-10-02 21:15:35 -0300

    Rename folders

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if ! [ -d $HOME/.ssh ]; then
+  echo "You forgot to restore your SSH key ;)"
+  exit 1
+fi
+
+cd setup
+
+./cleanup
+./add-repositories
+./packages
+./dotfiles
+./rubies
+./configs
+./notify-osd
+./vagrant-boxes
