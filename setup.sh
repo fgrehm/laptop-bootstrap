2844dc4aa45edba30f649c68d29c25eb7111db16
Fabio Rehm <fgrehm@gmail.com>
2015-09-03 10:25:21 -0300

    Android devices rules

--- setup.sh
+++ setup.sh
@@ -19,6 +19,20 @@ cd setup
 ./git-extras
 ./heroku
 
+# Based on https://help.ubuntu.com/community/AndroidSDK#Preparing_Hardware
+cat <<STR | sudo tee /etc/udev/rules.d/51-android.rules
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0bb4", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0502", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="12d1", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="1004", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="22b8", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="04e8", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0fce", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0489", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="18d1", SYMLINK+="android_adb", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="04e8", MODE="0666", GROUP="plugdev"
+STR
+
 # Add my user to vboxusers group so that USB devices work properly on VirtualBox
 sudo groupadd vboxusers
 sudo usermod -a -G vboxusers fabio
